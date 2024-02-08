bbab1dfc90e02f930a6f89ae476087dbd9e63983
Mulling <lmulling@proton.me>
2024-02-08 17:12:27 -0300

    Allow dyn linkage

--- config.mk
+++ config.mk
@@ -1,20 +1,22 @@
 # st version
 VERSION = 0.9.2
 
+LINKAGE ?= --shared
+
 # paths
 PREFIX = /usr/local
 MANPREFIX = $(PREFIX)/share/man
 
-INCS = `pkg-config --cflags x11        --static` \
-	   `pkg-config --cflags xft        --static` \
-       `pkg-config --cflags fontconfig --static` \
-       `pkg-config --cflags freetype2  --static`
-LIBS = `pkg-config --libs x11          --static` \
-	   `pkg-config --libs xft          --static` \
-       `pkg-config --libs fontconfig   --static` \
-       `pkg-config --libs freetype2    --static`
+INCS = `pkg-config --cflags x11        $(LINKAGE)` \
+	   `pkg-config --cflags xft        $(LINKAGE)` \
+       `pkg-config --cflags fontconfig $(LINKAGE)` \
+       `pkg-config --cflags freetype2  $(LINKAGE)`
+LIBS = `pkg-config --libs x11          $(LINKAGE)` \
+	   `pkg-config --libs xft          $(LINKAGE)` \
+       `pkg-config --libs fontconfig   $(LINKAGE)` \
+       `pkg-config --libs freetype2    $(LINKAGE)`
 
 # flags
 STCPPFLAGS = -DVERSION=\"$(VERSION)\" -D_XOPEN_SOURCE=600
-STCFLAGS = $(INCS) $(STCPPFLAGS) $(CPPFLAGS) $(CFLAGS) -static -flto -O3 -ffunction-sections
-STLDFLAGS = $(LIBS) $(LDFLAGS) -static -flto -O3 -Wl,--gc-sections
+STCFLAGS = $(INCS) $(STCPPFLAGS) $(CPPFLAGS) $(CFLAGS) -flto -O3 -ffunction-sections
+STLDFLAGS = $(LIBS) $(LDFLAGS) $(CFLAGS) -flto -O3 -Wl,--gc-sections
