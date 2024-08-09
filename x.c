f0ec4c083f301b0eb3554e411e54c31386907f2c
Lucas de Sena <lucas@seninha.org>
2024-08-09 13:33:47 +0200

    fix BadMatch error when embedding on some windows
    When embedded, st fails with BadMatch error if the embedder's window has
    non-default colormap/depth/visual.  This commit fixes that by creating
    st's window inside root and then reparent it into embedder.
    
    The reference window for dc.gc is also changed to match root's visuals.
    
    A similar commit had been made for dmenu[1].
    See this issue[2] on github for context.
    
    [1]: https://git.suckless.org/dmenu/commit/0fe460dbd469a1d5b6a7140d0e1801935e4a923b.html
    [2]: https://github.com/phillbush/xfiles/issues/47

--- x.c
+++ x.c
@@ -1133,11 +1133,11 @@ xicdestroy(XIC xim, XPointer client, XPointer call)
 void
 xinit(int cols, int rows)
 {
-    XGCValues gcvalues;
-    Cursor cursor;
-    Window parent;
-    pid_t thispid = getpid();
-    XColor xmousefg, xmousebg;
+	XGCValues gcvalues;
+	Cursor cursor;
+	Window parent, root;
+	pid_t thispid = getpid();
+	XColor xmousefg, xmousebg;
 
     if (!(xw.dpy = XOpenDisplay(NULL)))
         die("can't open display\n");
@@ -1172,21 +1172,24 @@ xinit(int cols, int rows)
         | ButtonMotionMask | ButtonPressMask | ButtonReleaseMask;
     xw.attrs.colormap = xw.cmap;
 
-    if (!(opt_embed && (parent = strtol(opt_embed, NULL, 0))))
-        parent = XRootWindow(xw.dpy, xw.scr);
-    xw.win = XCreateWindow(xw.dpy, parent, xw.l, xw.t,
-            win.w, win.h, 0, XDefaultDepth(xw.dpy, xw.scr), InputOutput,
-            xw.vis, CWBackPixel | CWBorderPixel | CWBitGravity
-            | CWEventMask | CWColormap, &xw.attrs);
+	root = XRootWindow(xw.dpy, xw.scr);
+	if (!(opt_embed && (parent = strtol(opt_embed, NULL, 0))))
+		parent = root;
+	xw.win = XCreateWindow(xw.dpy, root, xw.l, xw.t,
+			win.w, win.h, 0, XDefaultDepth(xw.dpy, xw.scr), InputOutput,
+			xw.vis, CWBackPixel | CWBorderPixel | CWBitGravity
+			| CWEventMask | CWColormap, &xw.attrs);
+	if (parent != root)
+		XReparentWindow(xw.dpy, xw.win, parent, xw.l, xw.t);
 
-    memset(&gcvalues, 0, sizeof(gcvalues));
-    gcvalues.graphics_exposures = False;
-    dc.gc = XCreateGC(xw.dpy, parent, GCGraphicsExposures,
-            &gcvalues);
-    xw.buf = XCreatePixmap(xw.dpy, xw.win, win.w, win.h,
-            DefaultDepth(xw.dpy, xw.scr));
-    XSetForeground(xw.dpy, dc.gc, dc.col[defaultbg].pixel);
-    XFillRectangle(xw.dpy, xw.buf, dc.gc, 0, 0, win.w, win.h);
+	memset(&gcvalues, 0, sizeof(gcvalues));
+	gcvalues.graphics_exposures = False;
+	dc.gc = XCreateGC(xw.dpy, xw.win, GCGraphicsExposures,
+			&gcvalues);
+	xw.buf = XCreatePixmap(xw.dpy, xw.win, win.w, win.h,
+			DefaultDepth(xw.dpy, xw.scr));
+	XSetForeground(xw.dpy, dc.gc, dc.col[defaultbg].pixel);
+	XFillRectangle(xw.dpy, xw.buf, dc.gc, 0, 0, win.w, win.h);
 
     /* font spec buffer */
     xw.specbuf = xmalloc(cols * sizeof(GlyphFontSpec));
